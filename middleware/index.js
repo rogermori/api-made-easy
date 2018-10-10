6211d395253f39f9854bd0ece76b52e17dbd007d
Roger Mori <moritues@unlv.nevada.edu>
2018-10-10 13:41:25 -0700

    Added ping and echo middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,18 @@
 const logBody = require('./logBody');
+
+const ping = ()=>(req, res, next)=> {
+  res.json({hi: 'I am alive'});
+  next();
+};
+
+const echo = ()=>(req, res, next)=>{
+  res.json(req.body);
+  next();
+};
+
+
 module.exports = {
   logBody,
+  ping,
+  echo,
 };
