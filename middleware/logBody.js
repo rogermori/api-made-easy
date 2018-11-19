feb0d654cc8443cdb2ff0196020d08ebf0fa5ff2
Roger Mori <moritues@unlv.nevada.edu>
2018-11-19 09:42:07 -0800

    Added precondition to logBody middleware

--- middleware/logBody.js
+++ middleware/logBody.js
@@ -1,6 +1,8 @@
 const logBody = (logger) => {
   return (req, res, next)=>{
-    let body = '';
+    let body = ''; let {protocol, originalUrl} = req;
+    logger(`${protocol}://${req.get('host')}${originalUrl}`);
+    if (!(req.method === 'POST' || req.method === 'PUT')) next();
     req.on('data', (chunk) => {
       body += chunk.toString(); // convert Buffer to string
     });
